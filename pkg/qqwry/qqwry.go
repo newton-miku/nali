9e7dcb73edd3db55a50628a818950c3f1270ffd0
newton_miku <29966140+newton-miku@users.noreply.github.com>
2024-06-03 01:15:54 +0800

    qqwry仅保留代理更新地址

--- pkg/qqwry/qqwry.go
+++ pkg/qqwry/qqwry.go
@@ -16,8 +16,8 @@ import (
 var DownloadUrls = []string{
 	"https://mirror.ghproxy.com/https://github.com/HMBSbige/qqwry/releases/latest/download/qqwry.dat",
 	"https://git.ddxnb.cn/https://github.com/HMBSbige/qqwry/releases/latest/download/qqwry.dat",
-	"https://gh-release.zu1k.com/HMBSbige/qqwry/qqwry.dat", // redirect to HMBSbige/qqwry
-	"https://github.com/HMBSbige/qqwry/releases/latest/download/qqwry.dat",
+	// "https://gh-release.zu1k.com/HMBSbige/qqwry/qqwry.dat", // redirect to HMBSbige/qqwry
+	// "https://github.com/HMBSbige/qqwry/releases/latest/download/qqwry.dat",
 	// Other repo:
 	// https://github.com/HMBSbige/qqwry
 	// https://github.com/metowolf/qqwry.dat
