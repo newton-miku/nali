a4eae787180d80a8495c4da49afcd4c150c29645
newton_miku <29966140+newton-miku@users.noreply.github.com>
2024-06-17 14:15:55 +0800

    Update update.go

--- cmd/update.go
+++ cmd/update.go
@@ -12,10 +12,10 @@ import (
 
 // updateCmd represents the update command
 var updateCmd = &cobra.Command{
-	Use:     "update [--db dbs -v]",
-	Short:   "update qqwry, zxipv6wry, ip2region ip database and cdn, update nali to latest version if -v",
-	Long:    `update qqwry, zxipv6wry, ip2region ip database and cdn. Use commas to separate. update nali to latest version if -v`,
-	Example: "nali update --db qqwry,cdn -v",
+	Use:     "update [--db dbs --v]",
+	Short:   "update qqwry, zxipv6wry, ip2region ip database and cdn, update nali to latest version if --v",
+	Long:    `update qqwry, zxipv6wry, ip2region ip database and cdn. Use commas to separate. update nali to latest version if --v`,
+	Example: "nali update --db qqwry,cdn --v",
 	Run: func(cmd *cobra.Command, args []string) {
 		DBs, _ := cmd.Flags().GetString("db")
 
